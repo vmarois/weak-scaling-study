7c5e403bb2236f099a9e5069f64e0ad818bf2c43
Vincent Marois <vincent.marois@ext.uni.lu>
2017-07-20 10:20:51 +0200

    Missing " ".

--- timings_plot.py
+++ timings_plot.py
@@ -38,7 +38,7 @@ os.system("mkdir xmlfiles")
 
 #CPU combinations to be studied.
 cores_combinations = ["0,1,2,3,4,5,6,7", "0,1,2,3", "4,5,6,7", "0,4", "0,1,4,5", "0,1,2,4,5,6"]
-cores_number = [8, 4, 4, 2, 4, 6]
+cores_number = ["8", "4", "4", "2", "4", "6"]
 
 #Generate the appropriate command for the weak-scaling test and then run it.
 #The number of dofs is constant and fixed to 124 250 (maximum value without crashing).
