77d7262f518cd67adbeb2ebbe0ad6cbe227b01fd
Vincent Marois <vincent.marois@ext.uni.lu>
2017-07-20 10:44:36 +0200

    Reduced max ndofs.

--- timings_plot.py
+++ timings_plot.py
@@ -43,7 +43,7 @@ cores_number = ["8", "4", "4", "2", "4", "6"]
 #Generate the appropriate command for the weak-scaling test and then run it.
 #The number of dofs is constant and fixed to 124 250 (maximum value without crashing).
 for i in range(len(cores_combinations)):
-	command = "mpiexec -n " + cores_number[i] + " -bind-to user:" + cores_combinations[i] + " ./demo_poisson --ndofs=124250 --xmlname=" + cores_combinations[i]
+	command = "mpiexec -n " + cores_number[i] + " -bind-to user:" + cores_combinations[i] + " ./demo_poisson --ndofs=124000 --xmlname=" + cores_combinations[i]
 	os.system(command)
 
 #Extracting results from the xml files.
