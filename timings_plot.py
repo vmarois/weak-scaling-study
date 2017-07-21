e27cd7be1d5a86219a6edbbd7f4b27f86b5de751
Vincent Marois <vincent.marois@ext.uni.lu>
2017-07-21 08:48:11 +0200

    Extracting more results.

--- timings_plot.py
+++ timings_plot.py
@@ -1,5 +1,6 @@
-#This script can be used to plot various results 
-#From the weak-scaling study.
+# This script can be used to plot various results 
+# From the weak-scaling study.
+
 from __future__ import print_function
 import os
 
@@ -34,10 +35,6 @@ def convert_table_to_dataframe(filename):
 
     return df
 
-directory = os.path.join(os.getcwd(), 'weak-scaling-demo', 'xmlfiles')
-if not os.path.exists(directory):
-    os.mkdir(directory)
-
 # CPU combinations to be studied.
 cores_combinations = [ range(0, 8), range(0, 4), range(4, 8), [0,4], [0,1,4,5], [0,1,2,4,5,6]]
 cores_number = [len(x) for x in cores_combinations]
@@ -51,15 +48,34 @@ def cores_to_str(cores_combination):
 
     return string
 
-function_spaces = np.zeros(len(cores_combinations))
+# Define lists to store results.
+assemble = np.zeros(len(cores_combinations))
+function_space = np.zeros(len(cores_combinations))
+solve = np.zeros(len(cores_combinations))
+total = np.zeros(len(cores_combinations))
 
 # Extracting results from the xml files.
 for i in range(0, len(cores_combinations)):
     timings = convert_table_to_dataframe("xmlfiles/timings_{}.xml".format(cores_to_str(cores_combinations[i])))
-    function_spaces[i] = timings.loc["wall tot"]["ZZZ Total"]
+    assemble[i] = timings.loc["wall tot"]["ZZZ Assemble"]
+    function_space[i] = timings.loc["wall tot"]["ZZZ FunctionSpace"]
+    solve[i] = timings.loc["wall tot"]["ZZZ Solve"]
+    total[i] = timings.loc["wall tot"]["ZZZ Total"]
 
+# Print some results.
+print("FunctionSpace timings : ")
 print(function_spaces)
 
+print("Assemble timings : ")
+print(assemble)
+
+print("Solve timings : ")
+print(solve)
+
+print("Total timings : ")
+print(total)
+
+print("Generating the plots...")
 sns.set_context("paper")
 colors = sns.color_palette("muted")
 
@@ -69,3 +85,4 @@ sns.barplot(function_spaces,cores)
 plt.xlabel(r"wall time (s)")
 plt.ylabel(r"cores combination")
 plt.savefig("total_times.pdf", bbox_inches='tight')
+print("Done. Results have been outputted to PDF.")
