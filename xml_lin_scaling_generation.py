bb15454efd7cfcb0ff1c8997006adf00c38fcf17
Vincent Marois <vincent.marois@ext.uni.lu>
2017-07-24 13:28:04 +0200

    Started scripts for linear scaling plotting.

---
+++ xml_lin_scaling_generation.py
@@ -0,0 +1,34 @@
+# This script runs a series of weak-scaling
+# tests and outputs the results to xml files.
+
+import os
+
+# CPU combinations to be studied.
+cores_combinations = [ [0,4], [0,1,4,5], [0,1,2,4,5,6], range(0,8)]
+cores_number = [len(x) for x in cores_combinations]
+
+# Create directory to store generated xml files.
+directory = os.path.join(os.getcwd(), 'xmlfiles')
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
+# Create directory to store outputs files (*pvd, *.pvtu *.xdmf, *.vtu)
+directory = os.path.join(os.getcwd(), 'output')
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
+def cores_to_str(cores_combination):
+    string = ""
+    for i, core in enumerate(cores_combination):
+        string += str(core)
+        if not i == len(cores_combination) - 1:
+            string += ","
+
+    return string
+
+#Generate the appropriate command for the weak-scaling test and then run it.
+#The number of dofs is constant and fixed to 120000 (maximum value without crashing).
+for i in range(len(cores_combinations)):
+	command = "mpiexec -n " + str(cores_number[i]) + " -bind-to user:" + cores_to_str(cores_combinations[i]) + " weak-scaling-demo/build/demo_poisson --ndofs=120000 --xmlname=" + cores_to_str(cores_combinations[i])
+	os.system(command)
+
